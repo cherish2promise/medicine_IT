b37ebfdbc556306f32c9f2be627a4644732fc36f
=HeonJun Lee <akilae2005@naver.com>
2020-01-14 03:08:14 +0900

    solution: 1.xls change csv 2. using sqlite

---
+++ app/src/main/java/com/example/medicine_it/DBManager.java
@@ -0,0 +1,29 @@
+package com.example.medicine_it;
+
+import android.content.Context;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class DBManager extends SQLiteOpenHelper {
+
+    public static final String  DB_NAME     = "medicine.db";
+    public static final int     DB_VERSION  = 1;
+
+
+    public DBManager( Context context)
+    {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+}
